f614b8dc7600945432f5188af322b264b8ab041b
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-05-09 17:44:44 +0800

    1.0

---
+++ TheLifeAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheLifeAssistant
+//
+//  Created by tztddong on 16/5/9.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
