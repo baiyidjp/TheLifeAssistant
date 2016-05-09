4cc5ea37db2c07cb4a4dcb54abaab921df33eaf3
baiyidjp <271524878@qq.com>
2016-05-09 21:36:23 +0800

    1.1--将需要的KEY和API 写入PCH文件

---
+++ TheLifeAssistant/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TheLifeAssistant
+//
+//  Created by I Smile going on 16/5/9.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
